2e68b36e499538d0da0ee3295f125ae0d9354a7b
Pcoder <115067774+Kenne400k@users.noreply.github.com>
2025-02-10 20:42:28 +0700

    Delete fca-h directory

--- fca-h/test/Database_Test.js
+++
@@ -1,4 +0,0 @@
-const DB = require('../Extra/Database');
-DB().set('a', 'b');
-console.log(DB().get('a')); // b
-console.log(DB().list());
--- fca-h/test/testv2.js
+++
@@ -1,3 +0,0 @@
-const crypto = require('crypto')
-const RandPass = Array.from({length: 101}, (_,i) => crypto.randomBytes(5).toString('hex'));
-console.log(RandPass)
